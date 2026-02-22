e843583e207069cf44f41874172d7997ca0b8b63
upendra sisodiya <sirfupendra@gmail.com>
2026-02-22 10:54:39 +0530

    first- commit

---
+++ src/main/java/com/parser/LLM/Data/LlmDataApplication.java
@@ -0,0 +1,13 @@
+package com.parser.LLM.Data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parser/LLM/Data/LlmDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parser.LLM.Data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
